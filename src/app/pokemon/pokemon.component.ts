00d0c33c325576748ed6a3c42505753092e827ca
ManishPoojary <manissh2327@gmail.com>
2019-04-01 17:05:39 +0530

    Load More, View All and Loading Added

---
+++ src/app/pokemon/pokemon.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { pokemonClass } from '../shared/AbstractClass/pokemonClass';
+import { Pokemon } from '../shared/models/pokemon';
+import { PokemonId } from '../shared/models/pokemonId';
+@Component({
+  selector: 'pokemon',
+  templateUrl: './pokemon.component.html',
+  styleUrls: ['./pokemon.component.scss']
+})
+export class PokemonComponent extends pokemonClass implements OnInit {
+  query: string;
+    
+    ngOnInit() {
+      this.getPokemons()
+      
+    }
+
+    loadMore(data){
+      
+    this.addPokemons(data);
+    // console.log(this.pokemons['results'].push({"name":"Manish","url":"https://pokeapi.co/api/v2/pokemon/23/"}))
+    }
+    viewAll(){
+      this.addPokemons('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=964');
+    }
+    
+}
